6a2769ca265a2f0f39e573de3cf45cbf59f4bd81
vitorazevedo.p7 <vitorazevedo.p7@gmail.com>
2026-05-04 00:19:19 -0300

    refactor: replace MapStruct annotations with manual mapper implementations

--- server/src/main/java/br/pucminas/lumen_coin_api/course/mapper/CourseMapper.java
+++ server/src/main/java/br/pucminas/lumen_coin_api/course/mapper/CourseMapper.java
@@ -2,9 +2,7 @@ package br.pucminas.lumen_coin_api.course.mapper;
 
 import br.pucminas.lumen_coin_api.course.dto.response.CourseResponse;
 import br.pucminas.lumen_coin_api.course.entity.Course;
-import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
 public interface CourseMapper {
     CourseResponse toResponse(Course course);
 }
